e94bc891c96c87d2b9e024f5e6b715c2f2925ec6
Tomas Afonso <tafonso@c2r2s10.42lisboa.com>
2025-10-06 17:32:54 +0100

    Fixed some pipex error (open fds) and cleaned so_long project

--- pipex/cleanup.c
+++ pipex/cleanup.c
@@ -6,7 +6,7 @@
 /*   By: tafonso <tafonso@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 16:26:49 by tafonso           #+#    #+#             */
-/*   Updated: 2025/09/10 16:39:55 by tafonso          ###   ########.fr       */
+/*   Updated: 2025/10/06 17:08:17 by tafonso          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,14 @@
 
 void	close_fds(t_pipe *pipex)
 {
-	close(pipex->fd[0]);
-	close(pipex->fd[1]);
-	close(pipex->infile);
-	close(pipex->outfile);
+	if (pipex->infile >= 0)
+		close(pipex->infile);
+	if (pipex->outfile >= 0)
+		close(pipex->outfile);
+	if (pipex->fd[0])
+		close(pipex->fd[0]);
+	if (pipex->fd[1])
+		close(pipex->fd[1]);
 }
 
 void	leave_program(char *message, int flag)
--- pipex/main.c
+++ pipex/main.c
@@ -6,7 +6,7 @@
 /*   By: tafonso <tafonso@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/09 13:44:27 by tafonso           #+#    #+#             */
-/*   Updated: 2025/09/10 16:40:18 by tafonso          ###   ########.fr       */
+/*   Updated: 2025/10/06 17:27:48 by tafonso          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,16 @@ int	main(int ac, char **av, char **envp)
 		leave_program("Usage: ./pipex file1 cmd1 cmd2 file2", 1);
 	pipex.infile = open(av[1], O_RDONLY);
 	pipex.outfile = open(av[4], O_CREAT | O_WRONLY | O_TRUNC, 0644);
-	if (pipex.infile < 0 || pipex.outfile < 0)
-		leave_program("Error opening files", 0);
 	if (pipe(pipex.fd) == -1)
+	{
+		close_fds(&pipex);
 		leave_program("Pipe error", 0);
+	}
+	if (pipex.infile < 0 || pipex.outfile < 0)
+	{
+		close_fds(&pipex);
+		leave_program("Error opening files", 0);
+	}
 	pipex.pid1 = fork();
 	child_process1(&pipex, av[2], envp);
 	pipex.pid2 = fork();
