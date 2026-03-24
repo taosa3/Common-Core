301652a1b3edf000d6f41089b24666456c1be79f
taosa <tomasafonso12050@gmail.com>
2026-03-24 18:37:58 +0000

    changes in CPP00

---
+++ CPP/CPP00/ex01/Contact.cpp
@@ -0,0 +1,13 @@
+#include "Contact.hpp"
+
+Contact::Contact(void)
+{
+	std::cout << "Contact Constructor called" << std::endl;
+	return ;
+}
+
+Contact::~Contact(void)
+{
+	std::cout << "Contact Destructor called" << std::endl;
+	return ;
+}
